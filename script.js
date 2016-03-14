c994dcdd4e2f4ef0b3dfd97649a5bcbbb1c82341
Currysacue <curry1284@gmail.com>
2016-03-14 10:05:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+<button onclick="myFunction()">Click me</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+</script>
 
